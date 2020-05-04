f4dd5e7711ddd68e5329daeb152b244d9e2bf4b1
huge <529581062@qq.com>
2020-05-04 20:07:00 +0800

    i++ 和 ++i

--- Basics/src/com/jihu/increment/Test1.java
+++ Basics/src/com/jihu/increment/Test1.java
@@ -1,4 +1,17 @@
 package com.jihu.increment;
 
+/**
+ * 如下代码运行结果
+ */
 public class Test1 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        i = i++; //i = 1
+        int j = i++; //j = 1;i = 2
+        int k = i+ ++i * i++; // k = 11 i= 4
+        System.out.println("i="+i); // 4
+        System.out.println("j=" + j); // 1
+        System.out.println("k=" + k);//11
+    }
 }
